7c842cb3910ec217d03a81fcce8697a637050d1d
Monia <moniam1126@gmail.com>
2023-09-14 00:30:00 +0300

    finished splash screen in light mode and dark mode.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,6 +4,6 @@ class HomeScreen extends StatelessWidget{
   static const String routeName = 'home_screen';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
